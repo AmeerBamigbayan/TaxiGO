4f9ad788d17f415104fec5cbf229585786caa1bb
Ameer_105 <ameerdamilola@gmail.com>
2022-06-18 13:40:38 +0100

    added js code

---
+++ create-pin/script.js
@@ -0,0 +1,13 @@
+const nums = document.querySelectorAll('.num')
+const form = document.querySelector('form')
+
+nums.forEach((num, index) => {
+    num.dataset.id = index
+    
+    num.addEventListener('keyup', () => {
+        if (num.value.length == 1) {
+            if (nums[nums.length - 1].value.length == 1) form.submit()
+            nums[parseInt(num.dataset.id) + 1].focus()
+        }
+    })
+})
